dde421e9a1363939b47e09d3edf12ac19d0ffb2c
Herb Miller <herb@bobbingwide.com>
2020-02-04 18:50:15 +0000

    For bw_attachments - Cater for parameter weirdness

--- shortcodes/oik-content.php
+++ shortcodes/oik-content.php
@@ -19,10 +19,29 @@ function oik_content_block( $attributes ) {
 	unset( $attributes['content'] );
 	unset( $attributes['parameters'] );
 
+    /**
+     * We need to set defaults to the same values as in the browser.
+     */
+    $attributes = oik_content_block_attributes( $attributes );
 	$attributes['post_parent'] = bw_array_get( $attributes, 'post_parent', '0');
+	$attributes['orderby'] = bw_array_get( $attributes, 'orderby', 'date');
+	$attributes['order'] = bw_array_get( $attributes, 'order', 'desc');
 
+
+	switch ( $shortcode ) {
+	    case 'bw_attachments':
+	        if ( $attributes['post_parent'] !== '0') {
+                $attributes['post_parent'] = bw_current_post_id();
+            }
+	    	unset( $attributes['post_type']);
+	    	//unset( $attributes['post_parent']);
+	    	break;
+
+
+    }
+
+	$parameters = trim( $parameters );
 	if ( $parameters ) {
-		$parameters=trim( $parameters );
 		$extra_atts=shortcode_parse_atts( $parameters );
 		$attributes+=$extra_atts;
 	}
@@ -42,4 +61,9 @@ function oik_content_block( $attributes ) {
 	return $result;
 
 
+}
+
+function oik_content_block_attributes( $attributes ) {
+	$attributes = \oik\oik_blocks\oik_blocks_attribute_unset_or_trim( $attributes, 'post_parent' );
+	return $attributes;
 }
