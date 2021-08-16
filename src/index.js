38910d4d2c2ae6d4625d6e1423fb851f1c2f7471
Herb Miller <herb@bobbingwide.com>
2021-08-16 18:32:45 +0100

    Transfer oik/content-block to oik #34

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Imports blocks for oik-blocks
  *
- * Each block is in a separate folder
+ * Each block is in a separate folder within src
  *
  * @copyright (C) Copyright Bobbing Wide 2018, 2019, 2020, 2021
  * @author Herb Miller @bobbingwide
@@ -9,7 +9,6 @@
 import './oik-blockicon';    // Displays a selected BlockIcon
 import './oik-blockinfo';    // Displays a selected Block's information
 import './oik-blocklist';    // Displays a list of Blocks for a selected prefix
-import './oik-content';      // Dynamic content block
 import './oik-fields';       // From oik-fields - [bw_fields] and [bw_field] shortcode
 import './oik-nivo';         // From oik-nivo-slider nivo
 import './oik-person';       // From oik-user bw_user and bw_follow_me etc
