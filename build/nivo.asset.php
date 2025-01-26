6498614b03099318e2aef9ee231029ce225dc87b
Herb Miller <herb@bobbingwide.com>
2025-01-26 14:21:48 +0000

    Update build files for Nivo #66

---
+++ build/nivo.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2c142c6ecb136f2129d5');
